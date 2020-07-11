002a19251616c72783d4c57731e9535a3807c43a
DevBensmaine <bensmaine.hassan@gmail.com>
2020-07-11 10:59:57 +0100

    parsing api

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -3,32 +3,25 @@ import { PostsService } from 'src/app/services/posts.service';
 import { Observable } from 'rxjs';
 import { Post } from '../../interfaces/Post';
 
-
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  posts: Post[] = [];
+  errorMessage: string = '';
 
-  posts:Post[] = [];
-  errorMessage:string = "";
-
-  constructor(private postService:PostsService) { }
+  constructor(private postService: PostsService) {}
 
   ngOnInit(): void {
     this.getPostNow();
   }
 
-  getPostNow(){
-    this.postService.getPosts()
-        .subscribe(
-        {
-          next: posts => this.posts = posts,
-          error: err=>this.errorMessage = err
-        }
-
-          )
+  getPostNow() {
+    this.postService.getPosts().subscribe({
+      next: (posts) => (this.posts = posts),
+      error: (err) => (this.errorMessage = err),
+    });
   }
-
 }
